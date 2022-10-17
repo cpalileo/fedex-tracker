fb175e873f186bcae17cf1b7e7ca36451fe2763b
Christopher Palileo <cpalileo@gmail.com>
2022-10-17 15:16:30 -0700

    prep localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,16 @@ const searchButton = document.getElementById("searchBtn");
 const trackingNumber = document.getElementById("searchInput");
 let searchedNumber = "";
 
-searchButton.onclick = function () {
+searchButton.onclick = function (event) {
+  event.preventDefault();
   searchedNumber = trackingNumber.value;
-  alert(searchedNumber);
+  localStorage.setItem("searchedNumber", JSON.stringify(searchedNumber));
+  console.log("Saved " + searchedNumber);
+  //getRequest()
 };
 
 //Save to History
+
+//API request
+
+//Display Info
