9ccb671a9c1727413f11b415bc0448adf6b9dd0f
Christopher Palileo <cpalileo@gmail.com>
2022-10-19 15:22:11 -0700

    form button and list working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,35 @@
-const searchButton = document.getElementById("searchBtn");
-const trackingNumber = document.getElementById("searchInput");
-let searchedNumber = "";
+const trackingFormEl = $("#searchForm");
+const trackingListEl = $("#trackingList");
 
-searchButton.onclick = function (event) {
+function addTracking(event) {
   event.preventDefault();
-  searchedNumber = trackingNumber.value;
-  localStorage.setItem("searchedNumber", JSON.stringify(searchedNumber));
-  console.log("Saved " + searchedNumber);
-  //getRequest()
-};
 
-//Save to History
+  let trackedItem = $("input[name='searchInput']").val();
 
-//API request
+  if (!trackedItem) {
+    alert("Please enter a valid tracking number.");
+    return;
+  }
 
-//Display Info
+  let trackingItemEl = $(
+    "<li class='flex-row justify-space-between align-center'>"
+  );
+  trackingItemEl.text(trackedItem);
+
+  trackingItemEl.append(
+    "<button class='btn btn-danger btn-small delete-tracking-btn'>Remove</button>"
+  );
+
+  trackingListEl.append(trackingItemEl);
+
+  $("input[name='searchInput']").val("");
+}
+
+function removeTracking(event) {
+  let btnClicked = $(event.target);
+
+  btnClicked.parent("li").remove();
+}
+
+trackingFormEl.on("submit", addTracking);
+trackingListEl.on("click", ".delete-tracking-btn", removeTracking);
