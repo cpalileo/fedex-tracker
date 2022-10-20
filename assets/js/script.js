df278779930ffae971513789cec6938f17cc7e89
Christopher Palileo <cpalileo@gmail.com>
2022-10-20 15:45:47 -0700

    backup minor updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,12 @@ function addTracking(event) {
   event.preventDefault();
 
   let trackedItem = $("input[name='searchInput']").val();
+  let apiData = $("API Data");
+
+  // REGEX for FedEx Tracking Numbers
+  //   /(\b96\d{20}\b)|(\b\d{15}\b)|(\b\d{12}\b)/
+  //   /\b((98\d\d\d\d\d?\d\d\d\d|98\d\d) ?\d\d\d\d ?\d\d\d\d( ?\d\d\d)?)\b/
+  //   /^[0-9]{15}$/
 
   if (!trackedItem) {
     alert("Please enter a valid tracking number.");
@@ -14,15 +20,22 @@ function addTracking(event) {
   let trackingItemEl = $(
     "<li class='flex-row justify-space-between align-center'>"
   );
+
+  let trackingItemData = $(
+    "<p class='flex-row justify-space-between align-center'>This is a test</p>"
+  );
+
   trackingItemEl.text(trackedItem);
+  trackingItemData.text(apiData);
 
   trackingItemEl.append(
     "<button class='btn btn-danger btn-small delete-tracking-btn'>Remove</button>"
   );
 
-  trackingListEl.append(trackingItemEl);
+  trackingListEl.append(trackingItemEl, trackingItemData);
 
   $("input[name='searchInput']").val("");
+  $("API Data");
 }
 
 function removeTracking(event) {
